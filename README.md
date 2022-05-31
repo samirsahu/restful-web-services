bc205ab31bb0995d6b6cb16ff8bf2111f7c0a4af
Samir Sahu <samirsahu2k@yahoo.com>
2022-05-30 21:55:07 -0400

    fdsa

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-#restful-web-services
+# restful-web-services
 
-Learning Restful web services using Spring Boot.
+Learning Restful web services using Spring Boot from udemy.
--- src/main/java/com/sam/study/restfulwebservices/users/User.java
+++ src/main/java/com/sam/study/restfulwebservices/users/User.java
@@ -1,15 +1,20 @@
 package com.sam.study.restfulwebservices.users;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
 public class User {
     private int id;
-    @Size(min=2,message = "Name should be atleast 2 chars")
+    @Size(min = 2, message = "Name should be atleast 2 chars")
     private String name;
     @Past
     private Date birthDate;
+    @JsonIgnore
+    private String password;
+
 
     public User(int id, String name, Date birthDate) {
         this.id = id;
@@ -39,6 +44,14 @@ public class User {
 
     public void setBirthDate(Date birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     @Override
